b473683a28ef210d1c6bfba50ad05f5d7f9e75a8
ahmed brahmi <AhmedBrahmi001>
2025-01-22 17:18:53 +0100

    Initial commit for portfolio

---
+++ src/components/About/About.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.jpg")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I specialize in building modern, responsive, and visually
+                appealing user interfaces using technologies like React, Vue.js,
+                and Tailwind CSS to deliver exceptional user experiences.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have expertise in developing fast, secure, and scalable
+                back-end systems, utilizing tools like Node.js, Laravel, and
+                Spring Boot to create reliable APIs and robust architectures.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>
+                I have designed multiple cohesive design systems and attractive
+                landing pages, ensuring seamless navigation and an outstanding
+                user experience.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Ahmed brahmi</h1>
+        <p className={styles.description}>
+          Freshly graduated software engineer with a strong foundation in
+          full-stack development. I specialize in crafting intuitive web and
+          mobile applications. Passionate about DevOps, I leverage modern tools
+          to streamline development and deployment processes
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
